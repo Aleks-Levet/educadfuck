771a40be5349bab8c0d37702e01485ce0eab69ce
Aleks levet <alekslevet@gmail.com>
2025-10-21 03:46:48 +0200

    Delete OPEN_THIS_THING.bat

--- OPEN_THIS_THING.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM ============================================
-REM  Launch EducadFuck portable
-REM ============================================
-
-cd /d "%~dp0"
-.\python\python.exe fuckthisshit.py
-pause
