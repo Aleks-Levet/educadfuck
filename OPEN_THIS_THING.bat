40d7caf5745c61c5d7225bcc16587c9fe87d27a8
Aleks levet <alekslevet@gmail.com>
2025-10-21 03:10:14 +0200

    Add files via upload

---
+++ OPEN_THIS_THING.bat
@@ -0,0 +1,8 @@
+@echo off
+REM ============================================
+REM  Launch EducadFuck portable
+REM ============================================
+
+cd /d "%~dp0"
+.\python\python.exe fuckthisshit.py
+pause
